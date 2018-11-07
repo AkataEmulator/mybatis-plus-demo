e6d3d0a5309f0118daa8489f6db8667bc5a65ede
见贤不思齐 <1151143436@qq.com>
2018-11-07 16:48:52 +0800

    测试生成的代码是否有效

--- mybatis-plus-generator-demo/src/main/java/com/suns/mybatisplus/MybatisPlusGeneratorDemoApplication.java
+++ mybatis-plus-generator-demo/src/main/java/com/suns/mybatisplus/MybatisPlusGeneratorDemoApplication.java
@@ -1,9 +1,14 @@
 package com.suns.mybatisplus;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author 见贤不思齐
+ */
 @SpringBootApplication
+@MapperScan(value = "com.suns.mybatisplus.mapper")
 public class MybatisPlusGeneratorDemoApplication {
 
     public static void main(String[] args) {
--- mybatis-plus-generator-demo/src/main/java/com/suns/mybatisplus/controller/UserController.java
+++ mybatis-plus-generator-demo/src/main/java/com/suns/mybatisplus/controller/UserController.java
@@ -1,20 +1,29 @@
 package com.suns.mybatisplus.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import com.suns.mybatisplus.entity.User;
+import com.suns.mybatisplus.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
- *  前端控制器
+ * 前端控制器
  * </p>
  *
  * @author suns
  * @since 2018-11-07
  */
 @RestController
-@RequestMapping("/mybatisplus/user")
+@RequestMapping("/userInfo")
 public class UserController {
+
+    @Autowired
+    private IUserService iUserService;
+
+    @GetMapping("/one")
+    public User findOne(@RequestParam(value = "id") Long id) {
+        return iUserService.getById(id);
+    }
 
 }
